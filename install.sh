041ab91d11fc51349ddd69df7985430bc46e5ff7
Tim Heuett <tim.heuett@gmail.com>
2017-08-26 09:50:29 -0700

    alter install script to use symlinks

--- install.sh
+++ install.sh
@@ -1,3 +1,14 @@
 #! /bin/bash
 
-cp -i src/.{bash_aliases,bash_profile,bashrc} ~/
+src_files='bashrc bash_aliases bash_profile'
+
+echo 'Backing up old files...'
+for f in $src_files; do mv -v ~/.$f ~/.${f}_backup; done
+
+echo 'Symlinking new files...'
+for f in $src_files; do ln -vs `pwd`/src/.$f ~/; done
+
+echo 'Sourcing new files...'
+source ~/.bash_profile
+
+echo 'Done!'
