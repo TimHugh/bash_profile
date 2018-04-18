27a8fc2ef8dd17998f1e1bb9ec11a86017aace90
Tim Heuett <tim.heuett@gmail.com>
2018-04-17 18:05:16 -0700

    fixes jira command check for aliases

--- src/bash_profile.d/jira.sh
+++ src/bash_profile.d/jira.sh
@@ -1,6 +1,6 @@
 # config for jira cli
 # https://github.com/sumoheavy/jira-ruby
-if [ -x "#(command -v jira)" ]; then
+if [ -x "$(command -v jira)" ]; then
   alias jql='jira issue jql'
   # case insensitive issue search
   function j {
